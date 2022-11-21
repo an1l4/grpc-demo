27db813cd94a0b3d6a4010ab1dffb9a14b9a4a7e
Anila Soman <1995anila@gmail.com>
2022-11-21 23:22:06 +0530

    grpc-demo

---
+++ chat/chat.go
@@ -0,0 +1,20 @@
+package chat
+
+import (
+	"log"
+
+	"golang.org/x/net/context"
+)
+
+type Server struct {
+}
+
+// mustEmbedUnimplementedChatServiceServer implements ChatServiceServer
+func (*Server) mustEmbedUnimplementedChatServiceServer() {
+	panic("unimplemented")
+}
+
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+	log.Printf("Received message body from client:%s", message.Body)
+	return &Message{Body: "Hello from the server"}, nil
+}
